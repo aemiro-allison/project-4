5edc690b6dbf1b221d44ad21dd772af68acc5cf0
Aemiro Allison <aemiro.allison@outlook.com>
2017-10-17 10:07:54 -0400

    add migration and seed files

---
+++ db/migrations/migration10152017.sql
@@ -0,0 +1,21 @@
+\c on_track;
+
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE groups (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INT REFERENCES users(id),
+  attributes JSONB NOT NULL
+);
+
+CREATE TABLE tasks (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  group_id INT REFERENCES groups(id),
+  user_id INT REFERENCES users(id),
+  attributes JSONB NOT NULL
+);
